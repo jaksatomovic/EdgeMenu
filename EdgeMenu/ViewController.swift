3f79b5b1c9a7b830b466f74dfcc5af97bfd2ccc4
Jaksa Tomovic <jaksa.tomovic@gmail.com>
2017-12-04 12:16:57 +0100

    Initial Commit

---
+++ EdgeMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EdgeMenu
+//
+//  Created by Jakša Tomović on 04/12/2017.
+//  Copyright © 2017 Jakša Tomović. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
